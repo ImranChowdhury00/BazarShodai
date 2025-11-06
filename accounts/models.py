ffaec6fe37fdaec261faa743990209090349eae7
Imran <imranchowdhury144@gmail.com>
2025-11-06 21:22:13 +0600

    account section

---
+++ accounts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    is_verified = models.BooleanField(default=False)
+    address_1 = models.CharField(max_length=100, null=True, blank=True)
+    address_2 = models.CharField(max_length=100, null=True, blank=True)
+    profile_picture = models.ImageField(upload_to='profile_picture', null=True, blank=True)
+    city = models.CharField(max_length=20, blank=True)
+    state = models.CharField(max_length=20, blank=True)
+    country = models.CharField(max_length=20, blank=True)
+    mobile = models.CharField(max_length=15,null=True, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    username = None
+
+    objects = CustomUserManager()
---
+++ accounts/views.py
@@ -0,0 +1,58 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from .forms import CustomUserRegistrationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserRegistrationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+    else:
+        form = CustomUserRegistrationForm()
+    return render(request, 'accounts/signup.html', {'form':form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, email= email, password= password)
+        if user is not None:
+            login(request, user)
+            messages.success("you have succcessfully logged in")
+        else:
+            messages.error("Invalid email or password")
+    else:
+        form = AuthenticationForm()
+        return render(request, 'accounts/login.html', {'form':form})
+    
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect('signup')
+
+
+@login_required
+def profile(request):
+    user = request.user
+    if request.method == 'POST':
+        user.email = request.POST.get("email", user.email)
+        user.mobile = request.POST.get("mobile", user.mobile)
+        user.address_1 = request.POST.get("address_line_1", user.address_1)
+        user.address_2 = request.POST.get("address_line_2", user.address_2)
+        user.city = request.POST.get("city", user.city)
+        user.state = request.POST.get("state", user.state)
+        user.country = request.POST.get("country", user.country)
+        user.save()
+
+        return redirect('profile')
+    context = {
+        'user_info': user
+    }
+    return render(request, 'accounts/profile.html', context)
