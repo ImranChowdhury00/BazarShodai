a5dd9be2c268c285209491b0afdb866be5241163
Imran <imranchowdhury144@gmail.com>
2025-11-09 23:00:22 +0600

    feat(carts): added cart app

--- BazarShodai/settings.py
+++ BazarShodai/settings.py
@@ -13,9 +13,16 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
+import environ
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -39,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'products'
+    'products',
+    'carts'
 ]
 
 MIDDLEWARE = [
@@ -66,6 +74,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'carts.context_processors.counter'
             ],
         },
     },
@@ -139,3 +148,5 @@ AUTHENTICATION_BACKENDS = [
     "accounts.authentication.EmailBackend",
     "django.contrib.auth.backends.ModelBackend",
 ]
+
+DELIVERY_CHARGE = env("DELIVERY_CHARGE")
--- BazarShodai/urls.py
+++ BazarShodai/urls.py
@@ -19,6 +19,7 @@ from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
     path('', include('products.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('carts/', include('products.urls')),
 ]
